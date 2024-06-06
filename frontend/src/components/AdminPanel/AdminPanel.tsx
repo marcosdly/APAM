b9317be8f784c7e23d8820d549bd0f23044c1193
Marcos Bião <dev.marcosdly@gmail.com>
2024-06-06 00:44:45 -0300

    feat(admin): page title and banner structure and style

--- frontend/src/components/AdminPanel/AdminPanel.tsx
+++ frontend/src/components/AdminPanel/AdminPanel.tsx
@@ -1,17 +1,42 @@
 import { SidebarProvider } from '@/hooks/SidebarProvider';
 import { FC, HTMLAttributes } from 'react';
 import AdminHeader from '../AdminHeader/AdminHeader';
+import { AdminPanelProvider, useAdminPanel } from '@/hooks/AdminPanelProvider';
 
 import './_admin-panel.scss';
+
+const Banner: FC<HTMLAttributes<HTMLElement>> = () => {
+  const { settings } = useAdminPanel();
+
+  return !settings.bannerText ? undefined : (
+    <aside className="admin-panel__banner">
+      <h1 className="admin-panel__banner-text">{settings.bannerText}</h1>
+    </aside>
+  );
+};
+
+const PageTitle: FC<HTMLAttributes<HTMLHeadingElement>> = () => {
+  const { settings } = useAdminPanel();
+
+  return !settings.title ? undefined : (
+    <h1 className="admin-panel__page-title">{settings.title}</h1>
+  );
+};
 
 const AdminPanel: FC<HTMLAttributes<HTMLElement>> = ({ children }) => {
   return (
     <SidebarProvider>
       <AdminHeader />
       <AdminHeader sidebar={true} />
-      <article className="admin-panel__working-area">
-        <main className="admin-panel__content-frame">{children}</main>
-      </article>
+      <AdminPanelProvider>
+        <div className="admin-panel">
+          <Banner />
+          <article className="admin-panel__working-area">
+            <PageTitle />
+            <main className="admin-panel__content-frame">{children}</main>
+          </article>
+        </div>
+      </AdminPanelProvider>
     </SidebarProvider>
   );
 };
