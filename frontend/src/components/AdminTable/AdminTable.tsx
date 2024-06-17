f371a14f07eaafbdeef48b4f2397611a3225f043
Marcos Bião <dev.marcosdly@gmail.com>
2024-06-17 12:45:05 -0300

    feat(admin): admin table container and result counter style

--- frontend/src/components/AdminTable/AdminTable.tsx
+++ frontend/src/components/AdminTable/AdminTable.tsx
@@ -2,9 +2,15 @@ import { FC } from 'react';
 import { Table } from '../PaginatedTable';
 
 import './_paginated-table.scss';
+import ResultCounter from '../PaginatedTable/ResultCounter';
 
 const AdminTable: FC<unknown> = () => {
-  return <Table />;
+  return (
+    <div className="admin-table">
+      <Table />
+      <ResultCounter />
+    </div>
+  );
 };
 
 export default AdminTable;
