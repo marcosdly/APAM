4de3edadf45a27ac41998afefbf045b2107ea64a
Marcos Bião <dev.marcosdly@gmail.com>
2024-06-17 23:00:48 -0300

    feat(admin): table page selector style

--- frontend/src/components/AdminTable/AdminTable.tsx
+++ frontend/src/components/AdminTable/AdminTable.tsx
@@ -3,12 +3,14 @@ import { Table } from '../PaginatedTable';
 
 import './_paginated-table.scss';
 import ResultCounter from '../PaginatedTable/ResultCounter';
+import PageSelector from '../PaginatedTable/PageSelector';
 
 const AdminTable: FC<unknown> = () => {
   return (
     <div className="admin-table">
       <Table />
       <ResultCounter />
+      <PageSelector />
     </div>
   );
 };
