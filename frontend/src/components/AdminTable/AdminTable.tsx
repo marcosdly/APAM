cba452f7d1bac0592e345cc9bb15ef36f1113706
Marcos Bião <dev.marcosdly@gmail.com>
2024-06-17 23:31:52 -0300

    feat(AdminTable): components correctly spread across available space

--- frontend/src/components/AdminTable/AdminTable.tsx
+++ frontend/src/components/AdminTable/AdminTable.tsx
@@ -1,16 +1,19 @@
 import { FC } from 'react';
 import { Table } from '../PaginatedTable';
-
-import './_paginated-table.scss';
 import ResultCounter from '../PaginatedTable/ResultCounter';
 import PageSelector from '../PaginatedTable/PageSelector';
+
+import './_paginated-table.scss';
+import './_admin-table.scss';
 
 const AdminTable: FC<unknown> = () => {
   return (
     <div className="admin-table">
       <Table />
-      <ResultCounter />
-      <PageSelector />
+      <div className="admin-table__footer">
+        <ResultCounter />
+        <PageSelector />
+      </div>
     </div>
   );
 };
