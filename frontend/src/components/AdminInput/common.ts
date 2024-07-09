d3c2de3b37bffd884419bc83bfc50a077de469bc
Marcos Bião <dev.marcosdly@gmail.com>
2024-07-09 18:10:52 -0300

    feat(admin): file and image input

--- frontend/src/components/AdminInput/common.ts
+++ frontend/src/components/AdminInput/common.ts
@@ -8,3 +8,10 @@ export interface Placeholder {
 }
 
 export type CommonInputProps = InputProps & Placeholder;
+
+/**
+ * Blank image (base64) to use as source of a <img /> tag as a way remove the
+ * 'special' border that exists when none is set.
+ */
+export const blankImage: string =
+  'data:image/gif;base64,R0lGODlhAQABAIAAAAAAAP///yH5BAEAAAAALAAAAAABAAEAQAIBRAA7';
