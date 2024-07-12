a43fc1348accf2ebe7f6f246399fa17cdc0733db
Marcos Bião <dev.marcosdly@gmail.com>
2024-07-12 10:20:20 -0300

    feat(AdminPanel): using classes to signal state to css

--- frontend/src/components/AdminPanel/AdminPanel.tsx
+++ frontend/src/components/AdminPanel/AdminPanel.tsx
@@ -8,7 +8,7 @@ import './_admin-panel.scss';
 const Banner: FC<HTMLAttributes<HTMLElement>> = () => {
   const { settings } = useAdminPanel();
 
-  return !settings.bannerText ? undefined : (
+  return (
     <aside className="admin-panel__banner">
       <h1 className="admin-panel__banner-text">{settings.bannerText}</h1>
     </aside>
@@ -19,23 +19,43 @@ const Banner: FC<HTMLAttributes<HTMLElement>> = () => {
 const PageTitle: FC<HTMLAttributes<HTMLHeadingElement>> = () => {
   const { settings } = useAdminPanel();
 
-  return !settings.title ? undefined : (
-    <h1 className="admin-panel__page-title">{settings.title}</h1>
-  );
+  return <h1 className="admin-panel__page-title">{settings.title}</h1>;
 };
 
 const EdgeButtonContainer: FC<unknown> = () => {
+  const { settings } = useAdminPanel();
+
+  return (
+    <div className="admin-panel__edge-btn-container">
+      {settings.buttons!.left}
+      {settings.buttons!.middle}
+      {settings.buttons!.right}
+    </div>
+  );
+};
+
+const Consumer: FC<HTMLAttributes<HTMLElement>> = ({ children }) => {
   const { settings } = useAdminPanel();
 
   const hasAnyValid = (pojo: Record<string, ReactNode>) => {
     return 'left' in pojo || 'middle' in pojo || 'right' in pojo;
   };
 
-  return !settings.buttons || !hasAnyValid(settings.buttons) ? undefined : (
-    <div className="admin-panel__edge-btn-container">
-      {settings.buttons!.left}
-      {settings.buttons!.middle}
-      {settings.buttons!.right}
+  const withBanner = settings.bannerText ? 'with-banner' : '';
+  const withTitle = settings.title ? 'with-title' : '';
+  const withButton =
+    settings.buttons && hasAnyValid(settings.buttons) ? 'with-edge-button' : '';
+
+  return (
+    <div className={`admin-panel ${withBanner} ${withTitle}`}>
+      {withBanner && <Banner />}
+      <article className={`admin-panel__working-area ${withButton}`}>
+        {withTitle && <PageTitle />}
+        {withButton && <EdgeButtonContainer />}
+        <main className={`admin-panel__content-frame ${withButton}`}>
+          {children}
+        </main>
+      </article>
     </div>
   );
 };
@@ -46,14 +66,7 @@ const AdminPanel: FC<HTMLAttributes<HTMLElement>> = ({ children }) => {
       <AdminHeader />
       <AdminHeader sidebar={true} />
       <AdminPanelProvider>
-        <div className="admin-panel">
-          <Banner />
-          <article className="admin-panel__working-area">
-            <PageTitle />
-            <EdgeButtonContainer />
-            <main className="admin-panel__content-frame">{children}</main>
-          </article>
-        </div>
+        <Consumer>{children}</Consumer>
       </AdminPanelProvider>
     </SidebarProvider>
   );
